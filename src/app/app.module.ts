92cc001ae1b45bcf63a30bacb2e56cac6b348ebd
shwetamulik <shweta17798@gmail.com>
2020-10-28 21:44:04 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,33 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+import{BrowserAnimationsModule} from '@angular/platform-browser/animations'
+import {MatToolbarModule} from '@angular/material/toolbar'
+import {MatListModule} from '@angular/material/list';
+import {FlexLayoutModule} from '@angular/flex-layout'
+import 'hammerjs';
+import {MatCardModule} from '@angular/material/card'
+import {MatButtonModule} from '@angular/material/button'
+import {MatGridListModule} from '@angular/material/grid-list'
+import { MenuComponent } from './menu/menu.component';
+import { DishdetailComponent } from './dishdetail/dishdetail.component'
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    DishdetailComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    FlexLayoutModule,
+    MatListModule,
+    MatGridListModule,
+    MatCardModule,
+    MatButtonModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
