31a84fa1e99c7fdd253eb7ae6cc4b8dfcb652baa
shwetamulik <shweta17798@gmail.com>
2020-11-03 10:00:29 +0530

    SPA-part1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,9 @@ import { FooterComponent } from './footer/footer.component';
 import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component'
+import { DishService } from './services/dish.service';
+import { PromotionService } from './services/promotion.service';
+
 @NgModule({
   declarations: [
     AppComponent,
@@ -42,7 +45,9 @@ import { ContactComponent } from './contact/contact.component'
     MatButtonModule
 
   ],
-  providers: [],
+  providers: [
+  DishService,
+  PromotionService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
